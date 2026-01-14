d2aed09029cc36f958f2038c3ae843ad802a1d3a
Luan Lorenzo <luanlorenzo23@gmail.com>
2026-01-14 11:38:41 -0300

    Add required case for file input

--- file/file-upload.runbook.sh
+++ file/file-upload.runbook.sh
@@ -1,2 +1,2 @@
 echo '{{ .myimage | type "file" | description "file upload" | decodeb64 }}' > myrawfile.jpg
-echo '{{ .csvfile | type "file" | description "file upload" | decodeb64 }}' > myfile.csv
+echo '{{ .csvfile | type "file" | description "file upload" | required "CSV file is required" | decodeb64 }}' > myfile.csv
